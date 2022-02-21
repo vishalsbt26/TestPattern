c23d3dbc421268f91c3451dffcd8f82a4f213cc6
vishalsbt26 <vishal@sevenbits.in>
2022-02-21 15:53:25 +0530

    First Commit

---
+++ DesignPatterns/src/test/java/com/test/basicTest.java
@@ -0,0 +1,24 @@
+package com.test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class basicTest {
+	
+	WebDriver driver;
+	@Test
+	public void initBrowser() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		driver.get("https://google.com");
+	}
+
+}
